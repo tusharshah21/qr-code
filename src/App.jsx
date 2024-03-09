8e12c774dac3aa3b5b37562e8d574544dc79e8b5
tushar <tusharkumarshah14394@gmail.com>
2024-03-10 00:55:39 +0530

    finished

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Card1 from "./section/Card1.jsx"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      <Card1/>
+      </div>
+      {/* <div className="underline">hello</div> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/section/Card1.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import Frame from "../assets/Frame.png"
+export default function Card1() {
+  return (
+    <>
+    <div className="h-screen w-screen text-center mx-auto flex justify-center items-center">
+    <div className="shadow-lg cardo border-black md:w-[320px] w-[89%] bg-white rounded-[20px] p-4 text-[#111111]">
+        <img src={Frame} alt="blog" className="w-full rounded-lg" />
+        <h1 className="text-[#1F314F] text-[22px] font-bold py-4 px-2 leading-7">
+        Improve your front-end skills by building projects
+        </h1>
+        <p className="text-[#7D889E] pb-6 leading-5 font-normal px-4">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+        </div>
+      </div>
+    </>
+  )
+}
